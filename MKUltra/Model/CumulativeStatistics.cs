f2c0d355db7ea2c9a1c75d0d08cf71d84d223004
jmulrey <jackmulrey@gmail.com>
2019-12-06 11:33:37 -0500

    Removing duplicate method from child statistics class

--- MKUltra/Model/CumulativeStatistics.cs
+++ MKUltra/Model/CumulativeStatistics.cs
@@ -31,18 +31,5 @@ namespace MKUltra.Model
             set => SetProperty(ref _gamesLost, value);
         }
 
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        protected bool SetProperty<T>(ref T field, T newValue, [CallerMemberName]string propertyName = null)
-        {
-            if (!EqualityComparer<T>.Default.Equals(field, newValue))
-            {
-                field = newValue;
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-                return true;
-            }
-            return false;
-        }
-
     }
 }
